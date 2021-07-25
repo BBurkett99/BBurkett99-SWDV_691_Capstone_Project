f4e8189b90e3b1aab66a37843a08ce161b8b3f51
BBurkett99 <56207359+BBurkett99@users.noreply.github.com>
2021-07-25 15:23:26 -0400

    Update login-page.js

--- src/login-page.js
+++ src/login-page.js
@@ -1,3 +1,6 @@
+/*Login page until the database is up and running*/
+
+
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
@@ -8,9 +11,9 @@ loginButton.addEventListener("click", (e) => {
     const password = loginForm.password.value;
 
     if (username === "user" && password === "password") {
-        /*alert("You have successfully logged in.");
-        location.reload();*/
-        window.location.href = "file:///C:/Users/brdbu/Desktop/Maryville/SWDV%20691%20SOFTWARE%20CAPSTONE/Notes/Trial/home.html";
+        alert("You have successfully logged in.");
+        location.reload();
+       window.location.href = "http://trackingyourhealth/home.html/azurewebsites.net/";
     } else {
         loginErrorMsg.style.opacity = 1;
     }
